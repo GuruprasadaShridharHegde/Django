15329dc8bf062a4663a371933eecc92b477619eb
Guruprasada Shridhar Hegde <gshegde6@gmail.com>
2023-06-12 22:47:50 +0530

    Django Temp Engine start

--- home/views.py
+++ home/views.py
@@ -3,7 +3,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request): # this is a route
-    return render(request,"index.html")
+    peoples = [
+        {'name':'GuruBoy','age': 26},
+        {'name':'GalliBoy','age': 36},
+        {'name':'Vicky','age': 46},
+        {'name':'John','age': 16},
+        {'name':'Don','age': 66},
+    ]
+
+    return render(request,"index.html", context = {'peoples': peoples})
   #  return HttpResponse("""<h1>Hey I am a Django Server</h1>"
   #                      <p>Hey this is coming from server</p>
   #                      <h3>hope you are well</h3>
