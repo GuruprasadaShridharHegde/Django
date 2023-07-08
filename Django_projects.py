6dc2505b98a0488aebfa1f4a4532b25af7ee13f4
Guruprasada Shridhar Hegde <gshegde6@gmail.com>
2023-07-08 16:32:38 +0530

    Committed

---
+++ Django_projects.py
@@ -0,0 +1,8 @@
+# Django
+Best projects to learn in Django
+### Crud Operation -- To DO or Blog application
+### Authentication with email verification
+### Integrate payment gateway
+### Web Scraping
+### Integrate CELERY in Django --( Tis is Task Queuing )
+### Implement Websockets using Channels
